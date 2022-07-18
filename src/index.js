1821a117f13ce094800173e06d5491bfc62ac2b2
Weiss Luxford <weissluxford@gmail.com>
2022-07-18 21:33:57 +0500

    solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  console.log(str, bracketsConfig);
+    const replacer = (string) => {
+      return string === undefined ? 0
+      : string.replace(/\(\)|\[\]|\{\}|\|\||12|34|56|77|88/g, "");
+    }
+    const brackets = (matrix) => {
+      for(let i = 0; i < str.length; i++){
+          matrix = replacer(matrix)
+      }
+      return matrix.length === 0 
+    }
+    return str === undefined ? true : brackets(str)
 }
